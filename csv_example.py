e49a9d1b54486afe35936f63e0f1bf99f8cb39c4
Makrau <iago006@hotmail.com>
2016-12-09 16:04:41 -0200

    Little refactoring

--- csv_example.py
+++ csv_example.py
@@ -10,16 +10,38 @@ def generate_header_dict(header):
 
 	while(x < len(header)):
 		if header[x] in values_of_interest:
-			print(header[x] + ' in the position ' + str(x))
 			header_dict[header[x]] = x
 		x += 1
 
 	return header_dict
 
+def write_row(writers_dict, header_dict, row):
+	col_number = 0
+	filtred_row = []
+	for col in row:
+		if col_number in header_dict.values():
+			filtred_row.append(row[col_number])
+
+		col_number += 1
+	writers_dict['default'].writerow(filtred_row)
+
+def close_files(output_files_dict):
+	dict_values = list(output_files_dict.values())
+	print(len(dict_values))
+
+
+	x = 0
+
+	while(x < len(dict_values)):
+		dict_values[x].close()
+		x += 1
+
 def write_filtred_csv(reader):
 	status_change_flags = ['27', '28']
-	output_file = open('filtred_tickets.csv', 'wt')
-	writer = csv.writer(output_file, delimiter=';')
+	default_ouput_file = open('default_output.csv', 'wt')
+	output_files_dict = {'default' : default_ouput_file}
+	default_writer = csv.writer(output_files_dict['default'], delimiter=';')
+	writers_dict = {'default' : default_writer}
 	row_counter = 0
 
 	for row in reader:
@@ -28,22 +50,12 @@ def write_filtred_csv(reader):
 			header_dict = generate_header_dict(header)
 			row_counter += 1
 			header =  ['tn', 'date', 'history_type_id', 'name', 'service', 'create_time']
-			writer.writerow(header)
+			writers_dict['default'].writerow(header)
 
 		elif row[header_dict['history_type_id']] in status_change_flags:
-			col_number = 0
-			filtred_row = []
-			for col in row:
-				if col_number in header_dict.values():
-					print('value to be printed: ' + row[col_number])
-					filtred_row.append(row[col_number])
+			write_row(writers_dict, header_dict, row)
 
-				col_number += 1
-
-			print('filtred_row after for: ' + str(filtred_row))
-			writer.writerow(filtred_row)
-
-	output_file.close()
+	close_files(output_files_dict)
 
 def main(file_path):
 	csv_file = open(file_path, 'rt')
